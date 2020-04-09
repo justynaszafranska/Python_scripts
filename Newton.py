273dae4ced51d8969965a2a7ca13cdf454ae1351
justynaszafranska <40147440+justynaszafranska@users.noreply.github.com>
2020-04-09 16:28:01 +0200

    Update and rename styczne_Newtona.py to Newton.py

--- Newton.py
+++ Newton.py
@@ -2,11 +2,11 @@ import math
 
 eps=1.0e-8
 
-alfa=float(input("Podaj wartość alfy większej od zera: "))
-x0=float(input("Wprowadź punkt startowy (x0) różny od 1: "))
+alfa=float(input("Enter an alpha value greater than zero: "))
+x0=float(input("Enter start point (x0) different from 1: "))
 
 if alfa<0 or alfa <-x0-(math.pi/2):
-    print("Parament alfa przekracza granice funcji")
+    print("The alpha parameter exceeds the function limits")
     
 def f(x):
     return math.atan(x)+x+alfa
@@ -14,12 +14,12 @@ def fp(x):
     return (1/(1+x**2))+1
 
 
-def styczne_Newtona(x0, eps, f):
+def Newton(x0, eps, f):
     x1, f0, i=x0 -1, f(x0), 90
     while (i>0) and (abs(x1 - x0) > eps) and (abs(f0) > eps):
          f1 = fp(x0)
          if abs(f1) <eps:
-             print ("Zły punkt startowy")
+             print ("Bad starting point")
              i=0
              break
          x1=x0
@@ -27,11 +27,11 @@ def styczne_Newtona(x0, eps, f):
          f0=f(x0)
          i-=1
          if i == 0:
-             print("Przekroczony limit obiegów")
+             print("Circulation limit exceeded")
          if i>0 :
              print(x0)
     return x0
-print(styczne_Newtona(x0,eps,f))
+print(Newton(x0,eps,f))
              
              
-         
+        
