717e09e7a2023368adb3b1052106c13e591586f5
justynaszafranska <40147440+justynaszafranska@users.noreply.github.com>
2020-04-09 16:17:23 +0200

    Update and rename regresja_liniowa.py to linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -3,29 +3,29 @@ import matplotlib.pyplot as plt  # To visualize
 import pandas as pd  # To read data
 from sklearn.linear_model import LinearRegression ,ElasticNet
 
-#wczytanie danych csv
+#loading CSV data
 data = pd.read_csv('data.csv')
 
-#wartosc konwertowane do numpy array
+#convert to numpy array
 X = data.iloc[:, 0].values.reshape(-1, 1)
 Y = data.iloc[:, 1].values.reshape(-1, 1)  # -1 means that calculate the dimension of rows, but have 1 column
 linear_regressor = LinearRegression()  # create object for the class
 
-# wykonywanie regresji liniowej
+#linear regression
 linear_regressor.fit(X, Y)
 
 Y_pred = linear_regressor.predict(X)
 
-#wykres
+#plot
 plt.scatter(X, Y,  color='black')
 plt.plot(X, Y_pred, color='blue', linewidth=3)
 
 plt.xticks(())
 plt.yticks(())
-plt.title('Regresja liniowa')
+plt.title('Linear regression')
 plt.show()
 
-#Współczynnik determinacji R^2
+#coefficient of determination R square
 regrEN = ElasticNet(normalize=True)
 regrEN.fit(X,Y)
-print ('Współczynnik determinacji wynosi: ',regrEN.score(X,Y))
+print ('Coefficient of determination: ',regrEN.score(X,Y))
