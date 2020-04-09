c954ce1fb10097b29c6a62f6c2433c15dfa66e25
justynaszafranska <40147440+justynaszafranska@users.noreply.github.com>
2020-04-09 14:06:06 +0200

    Add files via upload

---
+++ przeszukiwanie_wszerz.py
@@ -0,0 +1,45 @@
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
+
+
+def bfs(graph, start):
+    visited, queue = set(), [start]
+    while queue:
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+    return visited
+
+bfs(graph, 'A') # {'B', 'C', 'A', 'F', 'D', 'E'}
+
+#zwraca wszystkie możliwe ścieżki między dwoma wierzchołkami,
+#z których pierwsza jest jedną z najkrótszych takich ścieżek
+def bfs_paths(graph, start, goal):
+    queue = [(start, [start])]
+    while queue:
+        (vertex, path) = queue.pop(0)
+        for next in graph[vertex] - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                queue.append((next, path + [next]))
+
+list(bfs_paths(graph, 'A', 'F')) 
+print('Ścieżki między wierzchołkami A i F: ',list(bfs_paths(graph, 'A', 'F')))
+
+
+#najkrotsza znaleziona sciezka
+def shortest_path(graph, start, goal):
+    try:
+        return next(bfs_paths(graph, start, goal))
+    except StopIteration:
+        return None
+
+shortest_path(graph, 'A', 'F') 
+next=shortest_path(graph, 'A', 'F')
+print('Najkrótsza ścieżka między A i F: ',next)
